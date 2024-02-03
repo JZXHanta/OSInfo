f4a9d4237be38abdfd2e45aa2850afa33ebf5340
Hunter <61375197+JZXHanta@users.noreply.github.com>
2024-02-02 23:10:54 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JZXHanta/OSInfo
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func getPlatform() string {
+	return runtime.GOOS
+}
+
+func windowsOS() {
+	cmd := "(Get-WmiObject -class Win32_OperatingSystem).Caption"
+	// cmd := "echo"
+	// arg := "[System.Environment]::OSVersion.Version.Build"
+	c := exec.Command("powershell", "-NoProfile", cmd)
+
+	stdout, err := c.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(string(stdout))
+}
+
+func moreInfo() {
+	switch runtime.GOOS {
+	case "linux":
+		fmt.Println("LINUX FUNCTION HERE")
+	case "windows":
+		windowsOS()
+	}
+}
+
+func main() {
+	fmt.Println(getPlatform())
+	moreInfo()
+}
